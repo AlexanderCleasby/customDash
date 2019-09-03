403473e180e988ca583e3b4b9fbeaf6e6805c660
alexandercleasby <cleasby.alex@gmail.com>
2019-09-02 20:04:23 -0400

    refined styles for the navbar.

--- client/src/component/navbar/index.js
+++ client/src/component/navbar/index.js
@@ -4,15 +4,15 @@ import {Navbar, NavbarBrand, NavItem, NavLink, Nav} from 'reactstrap'
 import {Link} from 'react-router-dom'
 
 export default  ()=>{
-    return <Nav color="dark">
-        <NavbarBrand>
-            <Link to="/">
-                Custom Dash
+    return <Nav color="dark" className="bg-dark">
+        <NavbarBrand className="text-light">
+            <Link className="text-light nav-link" to="/">
+            Custom Dash
             </Link>
         </NavbarBrand>
             <NavItem >
                 <NavLink>
-                    <Link to="/new" >
+                    <Link className="text-light nav-link" to="/new" >
                         New
                     </Link>
                 </NavLink>
