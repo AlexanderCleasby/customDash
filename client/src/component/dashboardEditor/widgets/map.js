d680a283aa289c81af80b690946e4fa3ce831550
alexandercleasby <cleasby.alex@gmail.com>
2019-09-08 11:55:26 -0400

    added necessary methods to map widget class.

--- client/src/component/dashboardEditor/widgets/map.js
+++ client/src/component/dashboardEditor/widgets/map.js
@@ -6,8 +6,17 @@ import { faMap } from '@fortawesome/free-solid-svg-icons'
 export default class MapWidget extends widget{
     constructor(){
         super()
+        this.state={
+            lon:null,
+            lat:null,
+            zoom:1
+        }
     }
     icon=faMap
+    type="map"
+    ops=()=>{
+        return {coords:{lon:this.state.lon,lat:this.state.lat},zoomLevel:1}
+    }
     renderBody(){
         return <div>map Specfic body goes here</div>
     }
