06574e1a4e1567cff11a14941396538b8c8ac8d4
alexandercleasby <cleasby.alex@gmail.com>
2019-09-09 20:23:17 -0400

    map display component added.

---
+++ client/src/component/display/widgets/map.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react';
+import { DecimalToPercent } from "../../../utility/decimalToPercent";
+
+
+export default  class MapWidgetDisplay extends Component  {
+    constructor(...props){
+        super()
+    }
+    
+    static defaultProps = {
+        center: {
+          lat: 59.95,
+          lng: 30.33
+        },
+        zoom: 11
+    };
+
+    widgetStyles = ()=>({
+            position:'absolute',
+            top:DecimalToPercent(this.props.x/this.props.dashHeight),
+            left:DecimalToPercent(this.props.y/this.props.dashWidth),
+            height:DecimalToPercent(this.props.width/this.props.dashWidth),
+            width:DecimalToPercent(this.props.height/this.props.dashHeight) 
+            /* TODO: the way height and width are handled by the Dashboard editor is clearly mixed up, when that is fixed this method will nees to be fixed */ 
+    })
+
+    render() {
+        console.log(this.props)
+        return (
+            <div style={this.widgetStyles()} >
+                <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
+                defaultCenter={this.props.ops.coords}
+                defaultZoom={this.props.zoom}
+                options={{disableDefaultUI: true}}
+                yesIWantToUseGoogleMapApiInternals
+                />
+            </div>
+          
+        );
+      }
+}
+
+
