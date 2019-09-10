46b278c61cfc6c5591479602960fdff66e2ab1dd
alexandercleasby <cleasby.alex@gmail.com>
2019-09-09 20:21:52 -0400

    first pass at display container.

--- client/src/component/display/index.js
+++ client/src/component/display/index.js
@@ -1,10 +1,48 @@
 import React,{ Component } from 'react';
-
+import Ticker from './widgets/ticker'
+import Map from './widgets/map'
+import './display.scss'
 export default class Display extends Component{
-    componentDidMount(){
-        console.log(this.props)
+
+    constructor(){
+        super()
+        this.state={
+            widgets:[],
+            dashHeight:5,
+            dashWidth:5
+        }
     }
+
+    componentDidMount(){
+
+        
+        fetch(`/dashboards/${this.props.match.params.id}/?token=${window.localStorage.dashToken}`)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res)
+            this.setState({widgets:this.createWidgets(res.widgets)})
+        })
+    }
+
+    createWidgets=(widgets)=>{
+        
+        return widgets.map((widget)=>{
+            widget.dashHeight=this.state.dashHeight
+            widget.dashWidth=this.state.dashWidth
+            switch (widget.widget_type) {
+                case "map":
+                    return <Map {...widget} />
+                default:
+                    return <div />
+            }
+        })
+    }
+
     render(){
-        return <div>Display!</div>
+        return (
+        <div className="display">
+            {this.state.widgets}
+        </div>
+        )
     }
 } 
