af21b097c50ab4ca3eff3a91d9d4231924d37f03
alexandercleasby <cleasby.alex@gmail.com>
2019-09-12 09:51:12 -0400

    added dit button to dashboard index.

--- client/src/component/home/dashboards.js
+++ client/src/component/home/dashboards.js
@@ -3,6 +3,14 @@ import {Button, ListGroup,ListGroupItem} from 'reactstrap'
 
 export default (props)=>{
     return <ListGroup>
-        {props.dashboards.map((dashboard,i)=><ListGroupItem key={i}>{dashboard.name}<Button style={{float:"right"}} color="success" href={`/display/${dashboard.id}`}>Display</Button></ListGroupItem>)}
+        {props.dashboards.map((dashboard,i)=>(
+        <ListGroupItem key={i}>{dashboard.name}
+            <Button style={{float:"right"}} color="success" href={`/display/${dashboard.id}`}>
+                Display
+            </Button>
+            <Button style={{float:"right"}} color="warning" href={`/edit/${dashboard.id}`}>
+                Edit
+            </Button>
+        </ListGroupItem>))}
         </ListGroup>
 }
