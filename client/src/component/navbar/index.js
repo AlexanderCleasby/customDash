f0bc4138118f9ecafcdbad18d055c0ede497aef2
alexandercleasby <cleasby.alex@gmail.com>
2019-09-14 13:39:01 -0400

    updated navbar to solve dom validation errors.

--- client/src/component/navbar/index.js
+++ client/src/component/navbar/index.js
@@ -4,19 +4,19 @@ import {Navbar, NavbarBrand, NavItem, NavLink, Nav} from 'reactstrap'
 import {Link} from 'react-router-dom'
 
 export default  ()=>{
-    return <Nav color="dark" className="bg-dark">
-        <NavbarBrand className="text-light">
-            <Link className="text-light nav-link" to="/">
-            Custom Dash
-            </Link>
-        </NavbarBrand>
-            <NavItem >
-                <NavLink>
-                    <Link className="text-light nav-link" to="/new" >
-                        New
-                    </Link>
-                </NavLink>
-            </NavItem>
-        </Nav>
+    return (
+        <Navbar color="dark" dark expand="md">
+            <Nav color="dark" className="bg-dark" navbar>
+                <NavbarBrand className="text-light" tag={Link} to="/">
+                    Custom Dash
+                </NavbarBrand>
+                <NavItem  >
+                    <NavLink className="text-light" tag={Link} to="/new">
+                        New    
+                    </NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    )
     
 }
