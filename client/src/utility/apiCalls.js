d41674ef7c6f5dd9457309bbeb19e2e95aaf9551
alexandercleasby <cleasby.alex@gmail.com>
2019-09-15 16:33:51 -0400

    added dashboardDetail caller to apiCalls.

--- client/src/utility/apiCalls.js
+++ client/src/utility/apiCalls.js
@@ -5,3 +5,11 @@ export function dashboardSummary() {
             .then(res => resolve(res))
     })
 }
+
+export function dashboardDetail(){
+    return new Promise((resolve, reject) => {
+        fetch(`/dashboards/?token=${window.localStorage.dashToken}&detail=true`)
+        .then(res => res.json())
+        .then(res => resolve(res))
+})
+}
