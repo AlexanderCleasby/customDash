a0e51e5c5f052185fc16d9f7376fcd7c8e8b1a4a
alexandercleasby <cleasby.alex@gmail.com>
2019-09-15 18:12:13 -0400

    use react-router link instead of href to navigate to edit and display pages.

--- client/src/component/home/dashboards.js
+++ client/src/component/home/dashboards.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Button, ListGroup,ListGroupItem} from 'reactstrap'
+import {Link} from 'react-router-dom'
 
 export default (props)=>{
     return <ListGroup>
         {props.dashboards.map((dashboard,i)=>(
         <ListGroupItem key={i}>{dashboard.name}
-            <Button style={{float:"right"}} color="success" href={`/display/${dashboard.id}`}>
+            <Button style={{float:"right"}} color="success" tag={Link} to={`/display/${dashboard.id}`}>
                 Display
             </Button>
-            <Button style={{float:"right"}} color="warning" href={`/edit/${dashboard.id}`}>
+            <Button style={{float:"right"}} color="warning" tag={Link} to={`/edit/${dashboard.id}`} >
                 Edit
             </Button>
         </ListGroupItem>))}
