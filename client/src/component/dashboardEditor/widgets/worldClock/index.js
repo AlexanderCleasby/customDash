0e58d8eab465c84a95e4157225eb4b7eeb3a526d
alexandercleasby <cleasby.alex@gmail.com>
2019-09-28 21:26:29 -0400

    implemented the display widget component for the world clock.

---
+++ client/src/component/dashboardEditor/widgets/worldClock/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+//import moment from "moment";
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment-timezone/builds/moment-timezone-with-data';
+import Widget from '../widget';
+
+
+export default class WorldClock extends Widget {
+    constructor(props){
+        super(props)
+        this.state={
+            ...this.state,
+            timeZone:""
+        }
+    }
+    selectable = [
+        "America/Los_Angeles",
+        "America/New_York",
+        "Asia/Shanghai"
+    ]
+    displayZone=(zone)=>zone.split('/')[1].replace("_"," ")
+    ops=()=>({timeZone:this.state.timeZone})
+    icon=faClock
+    renderBody(){
+        return (
+            <div>
+                <select name="timeZone" onChange={this.valChange} >
+                    {this.selectable.map((zone,i)=><option key={i} value={zone}>{this.displayZone(zone)}</option>)}
+                </select>
+            </div>
+            )
+    }
+    
+}
