ec220e41017f771185250a9483ef2e46a68dd3f6
alexandercleasby <cleasby.alex@gmail.com>
2019-12-17 16:36:50 -0500

    re-wrote widget as a HOC accepting a wrappedWidget and aditonal widgetAttributes.

--- client/src/component/dashboardEditor/widgets/widget.js
+++ client/src/component/dashboardEditor/widgets/widget.js
@@ -5,73 +5,75 @@ import { faTrashAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 import "./widget.scss";
 
-export default class Widget extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      x:this.props.x||null,
-      y:this.props.y||null,
-      width:this.props.width||2,
-      height:this.props.height||1
-  };
-  }
-  
-  dimChange=(e)=>{
-    this.setState({[e.target.name]:parseInt(e.target.value)},this.props.forceUpdate)
-  }
+export default function Widget(WrappedWidget,widgetAttributes){
+  return  class Widget extends Component {
+    constructor(props) {
+      super(props);
 
-  componentDidMount(){
-    if (this.props.addToPlaced){
-      this.props.addToPlaced(this)}
-  }
-  
+      this.state = {
+        x:this.props.x||null,
+        y:this.props.y||null,
+        width:this.props.width||2,
+        height:this.props.height||1
+    };
+    }
 
-  valChangeInt=(e)=>{
-    this.setState({[e.target.name]:parseInt(e.target.value)})
-  }
+    dimChange=(e)=>{
+      this.setState({[e.target.name]:parseInt(e.target.value)},this.props.forceUpdate)
+    }
 
-  valChange=(e)=>this.setState({[e.target.name]:e.target.value})
+    componentDidMount(){
+      if (this.props.addToPlaced){
+        this.props.addToPlaced(this)}
+    }
 
-  valChangeFloat=(e)=>this.setState({[e.target.name]:parseFloat(e.target.value)})
 
-  render() {
-    //React throws out the drag
-      return (
-        <div className="widget">
-          <div
-            className="widgetHeader"
-          >
-            <div className="logo"
-                          draggable
-                          onDragStart={e=>this.props.handleDragStart(e,this)}
-                          onClick={e=>this.props.handleDragStart(e,this)}
-                          onDragEnd={this.props.hadnleDragEnd}
-                          onDrop={this.props.placeWidget}
-                          style={{backgroundColor:this.props.color}}> 
-            <FontAwesomeIcon
-              icon={this.icon}
-            />
-            </div>
-            <FontAwesomeIcon
-              icon={faArrowLeft}
-            />
-            Drag Me!
-            <div className="logo trash" onClick={(e)=>{this.props.deleteWidget(this.props.id);this.props.hadnleDragEnd(e);}}>
+    valChangeInt=(e)=>{
+      this.setState({[e.target.name]:parseInt(e.target.value)})
+    }
+
+    valChange=(e)=>this.setState({[e.target.name]:e.target.value})
+
+    valChangeFloat=(e)=>this.setState({[e.target.name]:parseFloat(e.target.value)})
+
+    render() {
+      //React throws out the drag
+        return (
+          <div className="widget">
+            <div
+              className="widgetHeader"
+            >
+              <div className="logo"
+                            draggable
+                            onDragStart={e=>this.props.handleDragStart(e,this)}
+                            onClick={e=>this.props.handleDragStart(e,this)}
+                            onDragEnd={this.props.hadnleDragEnd}
+                            onDrop={this.props.placeWidget}
+                            style={{backgroundColor:this.props.color}}> 
               <FontAwesomeIcon
-                icon={faTrashAlt}
+                icon={widgetAttributes.icon}
               />
+              </div>
+              <FontAwesomeIcon
+                icon={faArrowLeft}
+              />
+              Drag Me!
+              <div className="logo trash" onClick={(e)=>{this.props.deleteWidget(this.props.id);this.props.hadnleDragEnd(e);}}>
+                <FontAwesomeIcon
+                  icon={faTrashAlt}
+                />
+              </div>
             </div>
+            <WrappedWidget {...this.props} />
+            {(()=>{if(!this.props.hideDims){
+              return (<div>
+                <input name="width" type="number" onChange={this.dimChange} value={this.state.width} className="sizeInput" />
+                <input name="height" type="number" onChange={this.dimChange} value={this.state.height} className="sizeInput"  />
+              </div>)
+          }})()}
           </div>
-          {this.renderBody()}
-          {(()=>{if(!this.props.hideDims){
-            return (<div>
-              <input name="width" type="number" onChange={this.dimChange} value={this.state.width} className="sizeInput" />
-              <input name="height" type="number" onChange={this.dimChange} value={this.state.height} className="sizeInput"  />
-            </div>)
-        }})()}
-        </div>
-      );
+        );
 
+    }
   }
 }
