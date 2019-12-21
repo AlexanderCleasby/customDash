726eb9fdb4354820e178d97ccc7fb251ccb082ba
alexandercleasby <cleasby.alex@gmail.com>
2019-12-21 16:56:01 -0500

    updated Map widget to use changeOptions and handle props for coordinate props.

--- client/src/component/dashboardEditor/widgets/map.js
+++ client/src/component/dashboardEditor/widgets/map.js
@@ -4,33 +4,41 @@ import { faMap } from '@fortawesome/free-solid-svg-icons'
 
 
 class MapWidget extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            lng:this.props.ops ? this.props.ops.coords.lng : '',
-            lat:this.props.ops ? this.props.ops.coords.lat : '',
-            zoom:this.props.ops ? this.props.ops.zoomLevel : 1
-        }
-        
-    }
-    icon=faMap
-    type="map"
+
     ops=()=>{
         return {coords:{lng:this.state.lng,lat:this.state.lat},zoomLevel:1}
     }
+
+    changeCoords=(e)=>{
+        let coords = {...defaultCoords,...this.props.ops.coords}
+        this.props.changeOptions({coords:{...coords,[e.target.name]:e.target.value}})
+    }
+
+    valChangeFloat=(e)=>this.setState({[e.target.name]:parseFloat(e.target.value)})
+
+    coords=()=>({
+        lat: this.props.ops.coords ? this.props.ops.coords.lat : defaultCoords.lat,
+        lng: this.props.ops.coords ? this.props.ops.coords.lng : defaultCoords.lng
+    })
+
     render(){
         return (
         <div>
             <div className="widgetControl">
                 <label>Lat:</label>
-                <input name="lat" type="number" value={this.state.lat} onChange={this.valChangeFloat}  />
+                <input name="lat" type="number" value={this.coords().lat} onChange={this.changeCoords}  />
             </div>
             <div className="widgetControl">
                 <label>Lon:</label>
-                <input name="lng" type="number" value={this.state.lng} onChange={this.valChangeFloat}  />
+                <input name="lng" type="number" value={this.coords().lng} onChange={this.changeCoords}  />
             </div>
         </div>)
     }
 }
+
+const defaultCoords = {
+	lat: 40.7128,
+	lng: -74.006
+};
 
 export default widget(MapWidget,{icon:faMap})
